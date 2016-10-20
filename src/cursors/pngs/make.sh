b6ecc998a9069ad730e4f6715896993d941dc808
iainh <iain.heggie@gmail.com>
2016-10-20 15:49:03 -0400

    Add more cursors.

--- src/cursors/pngs/make.sh
+++ src/cursors/pngs/make.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 xcursorgen left_ptr.in ../../../limerick/cursors/left_ptr
+xcursorgen xterm.in ../../../limerick/cursors/xterm
+xcursorgen hand2.in ../../../limerick/cursors/hand2
+xcursorgen move.in ../../../limerick/cursors/move
+xcursorgen bottom_right_corner.in ../../../limerick/cursors/bottom_right_corner
+xcursorgen bottom_left_corner.in ../../../limerick/cursors/bottom_left_corner
+xcursorgen top_right_corner.in ../../../limerick/cursors/top_right_corner
+xcursorgen top_left_corner.in ../../../limerick/cursors/top_left_corner
+xcursorgen left_side.in ../../../limerick/cursors/left_side
+xcursorgen right_side.in ../../../limerick/cursors/right_side
+xcursorgen top_side.in ../../../limerick/cursors/top_side
+xcursorgen bottom_side.in ../../../limerick/cursors/bottom_side
